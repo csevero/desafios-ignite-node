c5a2fdded650484d01cc00d92ca6e956361c9969
kissinger156 <severo.e.carlos@gmail.com>
2021-07-04 22:35:05 -0300

    challenge 03

---
+++ desafio-ch03/src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    // Complete usando ORM
+    const user = (await this.repository.findOne({
+      where: { id: user_id },
+      relations: ["games"],
+    })) as User;
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("SELECT * FROM users order by first_name"); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      `SELECT * FROM users where first_name ILIKE '%${first_name}%' and last_name ILIKE '%${last_name}%'`
+    ); // Complete usando raw query
+  }
+}
